4efe8855c640352a734bdf64d6d7cdef1651f39e
Aryankanojia9654 <aryankanojia354@gmail.com>
2024-10-18 19:00:38 +0530

    backend started

---
+++ Backend/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { DB_NAME } from '../constants.js'
+dotenv.config()
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(` \n MongoBB connected !! DB HOST: ${
+            connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error" , error);
+        process.exit(1)
+    }
+}
+export default connectDB 
