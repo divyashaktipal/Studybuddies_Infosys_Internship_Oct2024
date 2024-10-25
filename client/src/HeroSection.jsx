e79318972ca482e02cc5f7444500a79eb678260e
anuragvermase <anuragver689@gmail.com>
2024-10-25 21:52:20 +0530

    add Updated HeroSection

--- client/src/HeroSection.jsx
+++ client/src/HeroSection.jsx
@@ -1,23 +1,25 @@
 // HeroSection.js
 
 import React from 'react';
-import './HeroSection.css';
-import heroImage from '../assets/heroSection.jpg'; 
+import './HeroSection.css';  // Import CSS
+import heroImage from './assets/heroSection.jpg'
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <div className="hero-text">
-        <h1>Create flashcards</h1>
-        <h2>
-          Share with friends, <br /> <br />
-          and explore study decks!
-        </h2>
-        <br />
-        <button className="hero-button">Create <span>&#43;</span></button> 
+      <div className="hero-content">
+        <h1>
+          Master Your <span>Knowledge</span> <br /> with Personalized Flashcards...
+        </h1>
+        <p>
+        Organize your learning, create custom decks, and craft flashcards to capture essential notes, ideas, and insights.
+        </p>
+        <div className="hero-buttons">
+          {/* <p><strong> "The more I learn, the more I realize how much I don't know."</strong> – Albert Einstein</p> */}
+        </div>
       </div>
       <div className="hero-image">
-        <img src={heroImage} alt="People collaborating" />
+        <img src={heroImage} alt="" />
       </div>
     </div>
   );
