081061c35a0c039aec43cc47c5295dffb80cfa74
Balaji <balajikarthik004@gmail.com>
2024-11-13 21:38:52 +0530

    bug fixed in reset password

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),pluginRewriteAll()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
