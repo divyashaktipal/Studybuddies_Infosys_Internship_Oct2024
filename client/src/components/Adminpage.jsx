d786206d635ef838e4c52c550fb974a9df8ae3da
20H71A1250 <vandhukanikicharla@gmail.com>
2024-11-14 23:44:35 +0530

    commit

--- client/src/components/Adminpage.jsx
+++ client/src/components/Adminpage.jsx
@@ -109,8 +109,8 @@ const Adminpagebody = () => {
         />
       </div>
       <div className={`hidden md:flex items-center gap-6 transition-transform duration-300 ${isOpen ? "open" : ""}`}>
-      <a href="/home" className="font-bold text-black hover:text-green-600 transition-colors duration-300">Home</a>
-<a href="/logout" className="font-bold text-black hover:text-green-600 transition-colors duration-300">Logout</a>
+      <a href="/home" className="font-bold text-black hover:text-green-700 transition-colors duration-300">Home</a>
+<a href="/logout" className="font-bold text-black hover:text-green-700 transition-colors duration-300">Logout</a>
 
       </div>
     
