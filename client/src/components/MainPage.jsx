67398b99afa5a92ce1ef7ed1ac1ed70fff53c1e8
jatin-sh01 <jatin80908@gmail.com>
2024-11-19 16:11:33 +0530

    fixing bugs

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -224,7 +224,7 @@ const MainPage = () => {
         <div className="container mx-auto text-center bg-gray-800">
           <p>&copy; 2024 Study Buddy. All Rights Reserved.</p>
           <div className="mt-2 space-x-4">
-            {["Privacy Policy", "Terms of Service", "Contact Us"].map(
+            {["Privacy Policy", "Terms-of-Service", "Contact Us"].map(
               (item) => (
                 <a
                   key={item}
