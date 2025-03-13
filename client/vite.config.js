cd9b8c5a44a8a1491618027132446480216a4b5c
Aryan Kanojia <aryankanojia354@gmail.com>
2025-03-13 11:22:17 +0000

    vite config changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pluginRewriteAll from 'vite-plugin-rewrite-all'
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), 
+    },
+  },
   server: {
     proxy: {
       '/api': {
